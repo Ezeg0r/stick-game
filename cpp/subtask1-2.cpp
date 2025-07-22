b82d2c97b5556711a44b50d144b1a24bf91dca33
Ezeg0r <153857791+Ezeg0r@users.noreply.github.com>
2025-07-22 18:12:26 +0300

    Update subtask1-2.cpp

--- cpp/subtask1-2.cpp
+++ cpp/subtask1-2.cpp
@@ -10,27 +10,26 @@ using namespace std;
 
 
 int bestStandardMove(int n, int a, int b){
-	int dp[n + 1];
-	int pr[n + 1];
-	memset(dp, 0, sizeof(dp));
-	memset(pr, 0, sizeof(pr));
-
-	for (int i = 1; i <= n; i++){
-		for (int j = a; j <= b; j++){
-			if (i - j >= 0){
-				if (dp[i - j] == 0){
-					dp[i] = 1;
-					pr[i] = j;
-					break;
-				}
-			}
-		}
-	}
-	return pr[n];
+    int dp[n + 1];
+    vector<int>pr[n + 1];
+    memset(dp, 0, sizeof(dp));
+    for (int i = 1; i <= n; i++){
+        for (int j = a; j <= b; j++){
+            if (i - j >= 0){ 
+                if (dp[i - j] == 0){
+                    dp[i] = 1;
+                    pr[i].push_back(j);
+                }
+            }
+        }
+    }
+    if (pr[n].empty())return 0;
+    return pr[n][rand() % pr[n].size()];
 }
 
 signed main(){
-	
-	cout << bestStandardMove(10, 1, 2);
-	return 0;
+    
+   cout <<  bestStandardMove(10, 4, 10);
+    
+    return 0;
 }
